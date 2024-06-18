cebb6e597cc377caf4da821be62e3305d70b0e1c
binhle <lexuanbinh220189@gmail.com>
2024-06-18 15:50:07 +0700

    init ci/cd

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.spec.ts$',
-    transform: {
-      '^.+\\.(t|j)s$': 'ts-jest'
-    },
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node'
-  };
-  
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'dist',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node'
+};
